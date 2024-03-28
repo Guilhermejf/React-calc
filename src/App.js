6e23adec6eb6b36cec2e66b968f5380998e8a677
Luiz Guilherme <contato.guilhermealmeida@gmail.com>
2024-03-28 17:03:22 -0300

    props in styled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Input from "./components/Input/";
 import Button from "./components/Button";
 
 function App() {
+  const color = "grey"
   return (
     <div>
       <Container>
@@ -11,33 +12,33 @@ function App() {
           <Column>
           
           <Row>
-            <Button label="%" />
-            <Button label="C" />
-            <Button label="<" />
-            <Button label="/" />
+            <Button label="%" color={color}/>
+            <Button label="C" color={color}/>
+            <Button label="<" color={color}/>
+            <Button label="/" color={color} />
           </Row>
           <Row>
             <Button label="7" />
             <Button label="8" />
             <Button label="9" />
-            <Button label="x" />
+            <Button label="x" color={color} />
           </Row>
           <Row>
             <Button label="4" />
             <Button label="5" />
             <Button label="6" />
-            <Button label="-" />
+            <Button label="-" color={color}/>
           </Row>
           <Row>
             <Button label="1" />
             <Button label="2" />
             <Button label="3" />
-            <Button label="+" />
+            <Button label="+" color={color}/>
           </Row>
           <Row>
-            <Button label="+/-" />
+            <Button label="+/-" color={color} />
             <Button label="0" />
-            <Button label="." />
+            <Button label="." color={color} />
             <Button label="=" color={"orange"} />
           </Row>
           </Column>
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ import { ButtonContainer } from './styles'
 
 const Button = ({label,color}) =>{
     return(
-        <ButtonContainer>
+        <ButtonContainer background={color}>
             {label}
         </ButtonContainer>
     )
